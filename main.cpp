d014568d6dfe527814a992c2902a2d660812124c
toannd1235 <37661811+toannd1235@users.noreply.github.com>
2018-04-17 10:45:23 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#define _WIN32_WINNT 0x0500
+#define W 40
+#define H 24
+#include <windows.h>
+#include <iostream>
+#include<conio.h>
+#include <thread>#include <cmath>
+using namespace std;
+#include "functions.h"
+#include "menu.h"
+#include "elements.h"
+#include "game.h"
+
+int main()
+{
+resizeScreen(200,100,600,600);
+thread second(listenInput);
+
+    while(1){
+    int selection = showMenu();
+    if(selection==1){
+        startGame();
+    }
+
+    else if(selection==2){
+        system("cls");
+        gotoXY(5,4);
+        cout<<"GOOD BYE!";
+        Sleep(2000);
+        system("cls");
+        break;
+    }
+
+    }
+
+   // second.detach();
+    return 0;
+}
